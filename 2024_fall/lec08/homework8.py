563555c2c2cd3e87e02bef93b8051b375407859a
Tian0719891 <tian895995552@gmail.com>
2025-01-09 00:02:45 +0800

    homework8

--- 2024_fall/lec08/homework8.py
+++ 2024_fall/lec08/homework8.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def dft_matrix(N):
-    '''
+    """
     Create a DFT transform matrix, W, of size N.
     
     @param:
@@ -10,6 +10,11 @@ def dft_matrix(N):
     @result:
     W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
            W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
-    '''
-    raise RuntimeError("You need to write this part")
-
+    """
+    # Create indices for rows and columns
+    k = np.arange(N).reshape((N, 1))  # Column vector
+    n = np.arange(N).reshape((1, N))  # Row vector
+    
+    # Compute the (k,n)^th element
+    W = np.exp(-2j * np.pi * k * n / N)
+    return W
